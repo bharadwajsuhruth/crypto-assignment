2b572245ac94a6ec329553d05843e46f26c94820
Bharadwaj Suhruth <108287375+bharadwajsuhruth@users.noreply.github.com>
2023-06-06 18:44:07 +0530

    affine_cipher.cpp

--- affine_cipher.cpp
+++ affine_cipher.cpp
@@ -1,69 +1,56 @@
-#include <iostream>
-#include <string>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int modInverse(int a, int m) {
-    a = a % m;
-    for (int x = 1; x < m; x++) {
-        if ((a * x) % m == 1) {
-            return x;
-        }
-    }
-    return -1;
+int modInverse(int k){
+	for(int i=0; i<26; i++){
+		int flag= (i*k)%26;
+		if(flag==1){
+			return i;
+		}
+	}
+	return -1;
 }
-
-string affineEncrypt(string plaintext, int a, int b) {
-    string ciphertext = "";
-    int n = plaintext.length();
-
-    for (int i = 0; i < n; i++) {
-        if (plaintext[i] != ' ') {
-            char encryptedChar = (a * (plaintext[i] - 'A') + b) % 26 + 'A';
-            ciphertext += encryptedChar;
-        } else {
-            ciphertext += ' ';
-        }
-    }
-
-    return ciphertext;
+string encrypt(string plain_text, int key1, int key2){
+	string cipher_text= "";
+	for(int i=0; i<plain_text.size(); i++){
+		if(isupper(plain_text[i])){
+			cipher_text+= char((key1* int(plain_text[i]-65)+ key2)%26 +65);
+		}else{
+			cipher_text+= char((key1* int(plain_text[i]-97)+ key2)%26 +97);
+		}
+	}
+	return cipher_text;
 }
-
-string affineDecrypt(string ciphertext, int a, int b) {
-    string plaintext = "";
-    int n = ciphertext.length();
-    int aInverse = modInverse(a, 26);
-
-    for (int i = 0; i < n; i++) {
-        if (ciphertext[i] != ' ') {
-            char decryptedChar = (aInverse * (ciphertext[i] - 'A' - b + 26)) % 26 + 'A';
-            plaintext += decryptedChar;
-        } else {
-            plaintext += ' ';
-        }
-    }
-
-    return plaintext;
+string decrypt(string cipher_text, int key1, int key2){
+	string plain_text= "";
+	int key_inverse= modInverse(key1);
+	if(key_inverse==-1){
+		return "Not exist";
+	}
+	cout<<"Key Inverse: "<<key_inverse<<endl;
+	for(int i=0; i<cipher_text.size(); i++){
+		if(isupper(cipher_text[i])){
+			plain_text+= char(((((cipher_text[i]-65- key2)%26+26)%26)*key_inverse)%26+65);
+		}else{
+			plain_text+= char(((((cipher_text[i]-97- key2)%26+26)%26)*key_inverse)%26+97);
+		}
+	}
+	return plain_text;
 }
-
-int main() {
-    string plaintext;
-    int a, b;
-
-    cout << "Enter plaintext: ";
-    getline(cin, plaintext);
-
-    cout << "Enter value for 'a': ";
-    cin >> a;
-
-    cout << "Enter value for 'b': ";
-    cin >> b;
-
-    string encryptedText = affineEncrypt(plaintext, a, b);
-    string decryptedText = affineDecrypt(encryptedText, a, b);
-
-    cout << "Encrypted text: " << encryptedText << endl;
-    cout << "Decrypted text: " << decryptedText << endl;
-
-    return 0;
+int main()
+{
+	string plain_text;
+	cout<<"Enter plain text: ";
+	cin>>plain_text;
+	int key1, key2;
+	cout<<"Enter first key: ";
+	cin>>key1;
+	cout<<"Enter second key: ";
+	cin>>key2;
+	
+	cout<<"\nEncryption:\n";
+	string cipher_text= encrypt(plain_text, key1, key2);
+	cout<<"Cipher: "<<cipher_text<<endl;
+	
+	cout<<"\nDecryption:\n";
+	cout<<"Plain Text: "<<decrypt(cipher_text, key1, key2)<<endl;
 }
